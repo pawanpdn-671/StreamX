d3b241db6ac4d1a5885d77534e46d50f8931cf96
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2021-10-19 07:27:11 +0530

    final v2

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import Modal from '../modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream, deleteStream } from '../../actions';
+import { Link } from 'react-router-dom';
 
-const StreamDelete = () => {
-    return (
-        <div>StreamDelete</div>
-    )
+class StreamDelete extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    renderContent() {
+        if(!this.props.stream) {
+            return 'Are you sure you want to delete this stream?'
+        }
+
+        return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`
+    }
+
+    renderActions() {
+        const { id } = this.props.match.params;
+
+        return(
+            <React.Fragment>
+                <button 
+                    className="ui button negative" 
+                    onClick={() => this.props.deleteStream(id)}
+                >
+                    Delete
+                </button>
+                
+                <Link to="/" className="ui button">Cancel</Link>
+            </React.Fragment>
+        );
+    };
+    
+    render() {
+        return (
+            <Modal
+                title="Delete Stream"
+                content={this.renderContent()}
+                actions={this.renderActions()}
+                onDismiss={() => history.push('/')}
+            />
+        )
+    }
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,8 +12,13 @@ class StreamList extends React.Component {
         if(stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-                    <button className="ui button negative">Delete</button>
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+                        Edit
+                    </Link>
+
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
+                        Delete
+                    </Link>
                 </div>
             );
         }
@@ -27,7 +32,9 @@ class StreamList extends React.Component {
 
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header">
+                            {stream.title}
+                        </Link>
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
