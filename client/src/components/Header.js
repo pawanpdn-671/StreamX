f5ac51c5a7553d7a991a6a12ab25a17191431a11
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2021-10-19 19:33:42 +0530

    adding video in the stream

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import GoogleAuth from './GoogleAuth';
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
-            <Link to="/" className="item">Streaming</Link>
+            <Link to="/" className="item">StreamX</Link>
             <div className="right menu">
                 <Link to="/" className="item">All Streams</Link>
                 <GoogleAuth />
